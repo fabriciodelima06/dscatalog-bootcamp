740b4ea3410c5085752b5a1a112b04c165b118c1
fabriciodelima06 <fabriciodelima06@gmail.com>
2021-06-04 12:08:19 -0300

    Implements OAuth2 authhorization server

--- backend/src/main/java/com/devsuperior/dscatalog/DscatalogApplication.java
+++ backend/src/main/java/com/devsuperior/dscatalog/DscatalogApplication.java
@@ -2,26 +2,12 @@ package com.devsuperior.dscatalog;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-
-import io.swagger.v3.oas.models.OpenAPI;
-import io.swagger.v3.oas.models.info.Info;
-import io.swagger.v3.oas.models.info.License;
 
 @SpringBootApplication
 public class DscatalogApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DscatalogApplication.class, args);
-	}
-	
-	@Bean
-	public OpenAPI customOpenAPI(){
-		return new OpenAPI().info(new Info()
-				.title("Aplicação criado no bootcamp da DevSuperio")
-				.version("1.0")
-				.termsOfService("http://swagger.io/terms")
-				.license(new License().name("Apache 2.0").url("http://springdoc.org")));
 	}
 
 }
